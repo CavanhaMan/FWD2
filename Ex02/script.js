8807e4bac5f97fd3a44c2d9e1406f34ced9ab259
CavanhaMan <cavanhaman@hotmail.com>
2019-05-23 22:35:48 -0300

    Add files via upload

--- Ex02/script.js
+++ Ex02/script.js
@@ -1,12 +1,17 @@
 /* Rodrigo CavanhaMan */
 clicked = true;
 $(document).ready(function (){
+	document.getElementById("imgmega").style.visibility = "hidden"; 
+	document.getElementById("imgmega").style.visibility = "hidden"; 
+	
 	$('button').hover(
-		function(){$(this).css("background-color", "red");}, 
-		function(){$(this).css("background-color", "#380606");}
+		function(){$(this).css("background-color", "#ffdb4d");}, 
+		function(){$(this).css("background-color", "#997a00");}
 	);
 	
 	$('#ex1').click(function(e){
+		document.getElementById("imgmega").style.visibility = 'visible';
+		document.getElementById("imgsorte").style.visibility = 'hidden';
 		$("#frametxt").attr("src", "MegaSena.txt");
 		$("#framehtml").attr("src", "MegaSena.html");
 	});
@@ -14,6 +19,8 @@ $(document).ready(function (){
 	$('#ex1').mouseout(function(e){$("#frametxt").attr("src", "");});
 
 	$('#ex2').click(function(e){
+		document.getElementById("imgmega").style.visibility = "hidden"; 
+		document.getElementById("imgsorte").style.visibility = "visible"; 
 		$("#frametxt").attr("src", "Sorteio.txt");
 		$("#framehtml").attr("src", "Sorteio.html");
 	});
