eec87ae620d7b250b38cb38534d7bf53da8b12b3
CavanhaMan <cavanhaman@hotmail.com>
2019-05-25 15:50:38 -0300

    Add files via upload

--- Ex02/script.js
+++ Ex02/script.js
@@ -1,32 +1,39 @@
 /* Rodrigo CavanhaMan */
-clicked = true;
 $(document).ready(function (){
-	document.getElementById("imgmega").style.visibility = "hidden"; 
-	document.getElementById("imgmega").style.visibility = "hidden"; 
-	
 	$('button').hover(
 		function(){$(this).css("background-color", "#ffdb4d");}, 
 		function(){$(this).css("background-color", "#997a00");}
 	);
 	
-	$('#ex1').click(function(e){
-		document.getElementById("imgmega").style.visibility = 'visible';
-		document.getElementById("imgsorte").style.visibility = 'hidden';
-		$("#frametxt").attr("src", "MegaSena.txt");
-		$("#framehtml").attr("src", "MegaSena.html");
+	//FUNÇÃO HOME
+	$('#ex0').click(function(e){
+		document.getElementById("imgshow").src=""; 
+		$("#frametxt").attr("src", "");
+		$("#framehtml").attr("src", "blank.html");
 	});
-	$('#ex1').mouseover(function(e){$("#frametxt").attr("src", "MegaSena.txt");});
-	$('#ex1').mouseout(function(e){$("#frametxt").attr("src", "");});
+	//$('#ex1').mouseover(function(e){$("#frametxt").attr("src", "");});
 
-	$('#ex2').click(function(e){
-		document.getElementById("imgmega").style.visibility = "hidden"; 
-		document.getElementById("imgsorte").style.visibility = "visible"; 
-		$("#frametxt").attr("src", "Sorteio.txt");
-		$("#framehtml").attr("src", "Sorteio.html");
+	//FUNÇÃO MEGASENA
+	$('#ex1').click(function(e){
+		document.getElementById("imgshow").src="megasena.png"; 
+		$("#frametxt").attr("src", "MegaSenaTxt.html");
+		$("#framehtml").attr("src", "MegaSena.html");
+		clicado=1;
 	});
-	$('#ex2').mouseover(function(e){$("#frametxt").attr("src", "Sorteio.txt");});
-	$('#ex2').mouseout(function(e){$("#frametxt").attr("src", "");});
+	//$('#ex1').mouseover(function(e){$("#frametxt").attr("src", "MegaSenaTxt.html");});
+
+	//FUNÇÃO SORTEIO
+	$('#ex2').click(function(e){
+		document.getElementById("imgshow").src="sorteio.png"; 
+		//document.getElementById("imgmega").style.visibility = "hidden"; 
+		$("#frametxt").attr("src", "SorteioTxt.html");
+		$("#framehtml").attr("src", "Sorteio.html");
+		clicado=2;
+	});
+	//$('#ex2').mouseover(function(e){$("#frametxt").attr("src", "SorteioTxt.html");});
 
 
 });
+//<img id="imgmega" src="megasena.png" alt="megasena">
+//<img id="imgsorte" src="sorteio.png" alt="sorteio" width="200px">
 
