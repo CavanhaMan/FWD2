a2d797486ae7563993f8826be150992ef0d99062
unknown <cavanhaman@hotmail.com>
2019-05-31 19:57:30 -0300

    Adicionando required para campos

--- Ex03_extra/script.js
+++ Ex03_extra/script.js
@@ -4,3 +4,7 @@ O usuário deverá ter selecionado um livro
 - Pelo menos uma opção deverá ter sido selecionada em “Como você conheceu o nosso site?”
 - O nome da pessoa não poderá estar em branco e deverá conter apenas letras
 */
+function myFunction() {
+  var x = document.getElementById("myRadio").required;
+  document.getElementById("demo").innerHTML = x;
+}
