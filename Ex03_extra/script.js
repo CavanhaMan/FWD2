2ed7c59a9de59cfff39195f73d06b93d91ae69f1
unknown <cavanhaman@hotmail.com>
2019-05-31 19:41:34 -0300

    Finalizando layout e inicio das autenticacoes

---
+++ Ex03_extra/script.js
@@ -0,0 +1,6 @@
+/*
+O usuário deverá ter selecionado um livro
+- Uma forma de pagamento deverá ter sido selecionada
+- Pelo menos uma opção deverá ter sido selecionada em “Como você conheceu o nosso site?”
+- O nome da pessoa não poderá estar em branco e deverá conter apenas letras
+*/
