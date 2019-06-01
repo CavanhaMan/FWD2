94e7dfbad8bdbfd985ff859e217fec4c558831ba
CavanhaMan <cavanhaman@hotmail.com>
2019-06-01 03:48:13 -0300

    Finalizando exercicio 03-03 com variantes

--- Ex03/Ex03-03.js
+++ Ex03/Ex03-03.js
@@ -4,19 +4,33 @@ $(document).ready(function (){
 		var numm2 = document.getElementById("txtNota2").value;
 		if(/[\a-zA-Z]/.test(numm1)){
 			alert("O campo apenas aceita números!" );
-			document.dobrar.txtNota1.focus();
+			document.getElementById("txtNota1").value="";
+			document.getElementById("txtNota1").focus();
 		}
 		if(/[\a-zA-Z]/.test(numm2)){
 			alert("O campo apenas aceita números!" );
-			document.dobrar.txtNota2.focus();
+			document.getElementById("txtNota2").value="";
+			document.getElementById("txtNota2").focus();
 		}
 		else if(numm1 == "" || numm1 == " "){
 			alert("Por favor preencha o campo!" );
-			document.dobrar.txtNota1.focus();
+			document.getElementById("txtNota1").value="";
+			document.getElementById("txtNota1").focus();
 		}
 		else if(numm2 == "" || numm2 == " "){
 			alert("Por favor preencha o campo!" );
-			document.dobrar.txtNota2.focus();
+			document.getElementById("txtNota2").value="";
+			document.getElementById("txtNota2").focus();
+		}
+		else if(numm1 > 50 || numm1 < 0){
+			alert("A nota deve ser entre 0 e 50!" );
+			document.getElementById("txtNota1").value="";
+			document.getElementById("txtNota1").focus();
+		}
+		else if(numm2 > 50 || numm2 < 0){
+			alert("A nota deve ser entre 0 e 50!" );
+			document.getElementById("txtNota2").value="";
+			document.getElementById("txtNota2").focus();
 		}
 		else{
 			var n1 = document.getElementById("txtNota1").value;
