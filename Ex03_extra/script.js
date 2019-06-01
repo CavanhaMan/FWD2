47fd488a0d8ce5ac686c4aab6a1e5ce11a269056
CavanhaMan <cavanhaman@hotmail.com>
2019-05-31 23:16:56 -0300

    Adicionado alguns padroes e mascara de CEP

--- Ex03_extra/script.js
+++ Ex03_extra/script.js
@@ -3,8 +3,39 @@ O usuário deverá ter selecionado um livro
 - Uma forma de pagamento deverá ter sido selecionada
 - Pelo menos uma opção deverá ter sido selecionada em “Como você conheceu o nosso site?”
 - O nome da pessoa não poderá estar em branco e deverá conter apenas letras
-*/
-function myFunction() {
+
+function cadastrar() {
   var x = document.getElementById("myRadio").required;
   document.getElementById("demo").innerHTML = x;
 }
+*/
+
+$("#txtCep").mask("00.000-000");
+
+document.forms["formCad"].onsubmit = function(){
+	if(this.checkValidity()){
+		alert("Formulário enviado com sucesso!");
+		return true;
+	}
+	else{
+		if(this["txtNome"].checkValidity === false)
+			alert("É necessário digitar um nome");
+		if(this["dropLivro"].checkValidity === false)
+			alert("É necessário escolher um livro");
+		if(this["radioPagto"].checkValidity === false)
+			alert("É necessário escolher uma forma de pagamento");
+		if(this["checkComo"].checkValidity === false)
+			alert("Por favor informe como conheceu nosso site");
+	}
+	return false;
+}
+
+
+
+/*
+<button onClick="dobrar()">
+
+function dobrar(){
+	var x = document.getElementById("valor").value;
+	document.getElementById("dobro").value = (x*2);
+}*/
