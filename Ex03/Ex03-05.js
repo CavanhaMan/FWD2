19926ea42e821a6ee52fd7673fe1e218437e63f7
CavanhaMan <cavanhaman@hotmail.com>
2019-06-01 04:12:43 -0300

    Finalizando exercicio 03-05 com tratamento de valores

--- Ex03/Ex03-05.js
+++ Ex03/Ex03-05.js
@@ -1,7 +1,31 @@
 function calcular(operador){
-	//var valor1 = document.getElementById('txtValor1').value;
-	//var valor2 = document.getElementById('txtValor2').value;
-	//var result = eval(valor1+operador+valor2);
-	//document.getElementById('txtResultado').value = result;
-	document.getElementById('txtResultado').value = eval(document.getElementById('txtValor1').value + operador + document.getElementById('txtValor2').value);
-}
+	var numm1 = document.getElementById("txtValor1").value;
+	var numm2 = document.getElementById("txtValor2").value;
+	if(/[\a-zA-Z]/.test(numm1)){
+		alert("O campo apenas aceita números!" );
+		document.getElementById("txtValor1").value="";
+		document.getElementById("txtValor1").focus();
+	}
+	if(/[\a-zA-Z]/.test(numm2)){
+		alert("O campo apenas aceita números!" );
+		document.getElementById("txtValor2").value="";
+		document.getElementById("txtValor2").focus();
+	}
+	else if(numm1 == "" || numm1 == " "){
+		alert("Por favor preencha o campo!" );
+		document.getElementById("txtValor1").value="";
+		document.getElementById("txtValor1").focus();
+	}
+	else if(numm2 == "" || numm2 == " "){
+		alert("Por favor preencha o campo!" );
+		document.getElementById("txtValor2").value="";
+		document.getElementById("txtValor2").focus();
+	}
+	else if(operador=="/" && numm2 == 0){
+		alert("Erro! Divisão por zero não pode ser efetuada!" );
+		document.getElementById("txtValor2").value="";
+		document.getElementById("txtValor2").focus();
+	}
+	else
+		document.getElementById('txtResultado').value = eval(document.getElementById('txtValor1').value + operador + document.getElementById('txtValor2').value);
+};
