59b62801430dff191efcd30429dff4bf7f3a401c
CavanhaMan <cavanhaman@hotmail.com>
2019-06-01 00:52:28 -0300

    Exercicio 03 Extra finalizado com sucesso

--- Ex03_extra/script.js
+++ Ex03_extra/script.js
@@ -3,39 +3,41 @@ O usuário deverá ter selecionado um livro
 - Uma forma de pagamento deverá ter sido selecionada
 - Pelo menos uma opção deverá ter sido selecionada em “Como você conheceu o nosso site?”
 - O nome da pessoa não poderá estar em branco e deverá conter apenas letras
-
-function cadastrar() {
-  var x = document.getElementById("myRadio").required;
-  document.getElementById("demo").innerHTML = x;
-}
 */
-
 $("#txtCep").mask("00.000-000");
 
 document.forms["formCad"].onsubmit = function(){
-	if(this.checkValidity()){
-		alert("Formulário enviado com sucesso!");
-		return true;
+	var nomee = document.formCad.txtNome.value
+	if(nomee=="" || nomee==" " || nomee.length < 1){
+		alert("É necessário digitar um nome!" );
+		document.formCad.txtNome.focus();
+		return false;
 	}
-	else{
-		if(this["txtNome"].checkValidity === false)
-			alert("É necessário digitar um nome");
-		if(this["dropLivro"].checkValidity === false)
-			alert("É necessário escolher um livro");
-		if(this["radioPagto"].checkValidity === false)
-			alert("É necessário escolher uma forma de pagamento");
-		if(this["checkComo"].checkValidity === false)
-			alert("Por favor informe como conheceu nosso site");
+	else if(/[\d]/.test(nomee)){
+		alert("O nome deve conter apenas letras!" );
+		document.formCad.txtNome.focus();
+		return false;
 	}
-	return false;
+	else if(/[\d]/.test(nomee)){
+		alert("O nome deve conter apenas letras!" );
+		document.formCad.txtNome.focus();
+		return false;
+	}
+	else if(document.formCad.dropLivro.value==""){
+		alert("É necessário escolher um livro!" );
+		document.formCad.dropLivro.focus();
+		return false;
+	}
+	else if(document.formCad.radioPagto.value==""){
+		alert("É necessário escolher uma forma de pagamento!" );
+		return false;
+	}
+	else if(document.formCad.ck1.checked==false && document.formCad.ck2.checked==false ){
+		alert("Por favor informe como conheceu nosso site!" );
+		return false;
+	}
+	alert("Cadastro efetuado com sucesso!");
+	return true;
 }
 
 
-
-/*
-<button onClick="dobrar()">
-
-function dobrar(){
-	var x = document.getElementById("valor").value;
-	document.getElementById("dobro").value = (x*2);
-}*/
