ae06a6b9435db7f9b584e34dc079d4baae8d3888
cavanhaman <cavanhaman@hotmail.com>
2019-06-10 16:28:07 -0300

    Alteracao de layout e verificacao de validacao

--- Ex04/ex04.js
+++ Ex04/ex04.js
@@ -9,18 +9,25 @@ document.getElementById("txtA").onfocus = function() {
     document.getElementById("mensagemA").innerHTML="";
 }
 document.getElementById("validaA").onclick = function() {
-    var dadoA = document.getElementById("txtA").value;
-    //var padrao5 = /^[2-4][0-9]|5[0-4]$/;
-    //var padraoA = /^\d{2}\/\d{2}\/(\d{2}|\d{4})$/;
-    var padraoA = /^([0-2]?[0-9]|3[0-1])\/(0?[0-9]|1[0-2])\/((19[0-9][0-9]|20?(0|1)[0-9])|[0-9][0-9])$/;
-    if (padraoA.test(dadoA)){
-        document.getElementById("mensagemA").innerHTML=" = Válido!";
-        document.getElementById("mensagemA").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemA").innerHTML=" = Inválido!";
-        document.getElementById("mensagemA").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaA").checked==false){
+		document.getElementById("validaA").checked=false;
+		document.getElementById("mensagemA").innerHTML="";
+	}
+	else{
+		document.getElementById("validaA").checked=true;
+		var dadoA = document.getElementById("txtA").value;
+		//var padrao5 = /^[2-4][0-9]|5[0-4]$/;
+		//var padraoA = /^\d{2}\/\d{2}\/(\d{2}|\d{4})$/;
+		var padraoA = /^([0-2]?[0-9]|3[0-1])\/(0?[0-9]|1[0-2])\/((19[0-9][0-9]|20?(0|1)[0-9])|[0-9][0-9])$/;
+		if (padraoA.test(dadoA)){
+			document.getElementById("mensagemA").innerHTML=" = Válido!";
+			document.getElementById("mensagemA").classList=("mensagem valido");
+		}
+		else{
+			document.getElementById("mensagemA").innerHTML=" = Inválido!";
+			document.getElementById("mensagemA").classList=("mensagem invalido");
+		}
+	}
 };
 /***LETRA B*******************************************************************/
 //Únicos formatos permitidos: xxx.xxx.xxx-xx. Onde x é um dígito.
@@ -29,16 +36,22 @@ document.getElementById("txtB").onfocus = function() {
     document.getElementById("mensagemB").innerHTML="";
 }
 document.getElementById("validaB").onclick = function() {
-    var dadoB = document.getElementById("txtB").value;
-    var padraoB = /^\d{3}.\d{3}.\d{3}-\d{2}$/;
-    if (padraoB.test(dadoB)){
-        document.getElementById("mensagemB").innerHTML=" = Válido!";
-        document.getElementById("mensagemB").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemB").innerHTML=" = Inválido!";
-        document.getElementById("mensagemB").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaB").checked==false){
+		document.getElementById("validaB").checked=false;
+		document.getElementById("mensagemB").innerHTML="";
+	}
+	else{
+		var dadoB = document.getElementById("txtB").value;
+		var padraoB = /^\d{3}.\d{3}.\d{3}-\d{2}$/;
+		if (padraoB.test(dadoB)){
+			document.getElementById("mensagemB").innerHTML=" = Válido!";
+			document.getElementById("mensagemB").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemB").innerHTML=" = Inválido!";
+			document.getElementById("mensagemB").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA C*******************************************************************/
 //Únicos formatos permitidos: IFTM-xxx/xxx-yy ou iftm-xxx/xxx-yy. Onde x é um dígito e y um caracter alfanumérico.
@@ -47,16 +60,22 @@ document.getElementById("txtC").onfocus = function() {
     document.getElementById("mensagemC").innerHTML="";
 }
 document.getElementById("validaC").onclick = function() {
-    var dadoC = document.getElementById("txtC").value;
-    var padraoC = /^IFTM-\d{3}\/\d{3}-\w{2}$/i;
-    if (padraoC.test(dadoC)){
-        document.getElementById("mensagemC").innerHTML=" = Válido!";
-        document.getElementById("mensagemC").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemC").innerHTML=" = Inválido!";
-        document.getElementById("mensagemC").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaC").checked==false){
+		document.getElementById("validaC").checked=false;
+		document.getElementById("mensagemC").innerHTML="";
+	}
+	else{
+		var dadoC = document.getElementById("txtC").value;
+		var padraoC = /^IFTM-\d{3}\/\d{3}-\w{2}$/i;
+		if (padraoC.test(dadoC)){
+			document.getElementById("mensagemC").innerHTML=" = Válido!";
+			document.getElementById("mensagemC").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemC").innerHTML=" = Inválido!";
+			document.getElementById("mensagemC").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA D*******************************************************************/
 //Único formato permitido: MT-xx.xxx-IFTM. Onde x é um dígito e 
@@ -66,16 +85,22 @@ document.getElementById("txtD").onfocus = function() {
     document.getElementById("mensagemD").innerHTML="";
 }
 document.getElementById("validaD").onclick = function() {
-    var dadoD = document.getElementById("txtD").value;
-    var padraoD = /^MT-\d{2}\.\d{3}-IFTM$/i;
-    if (padraoD.test(dadoD)){
-        document.getElementById("mensagemD").innerHTML=" = Válido!";
-        document.getElementById("mensagemD").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemD").innerHTML=" = Inválido!";
-        document.getElementById("mensagemD").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaD").checked==false){
+		document.getElementById("validaD").checked=false;
+		document.getElementById("mensagemD").innerHTML="";
+	}
+	else{
+		var dadoD = document.getElementById("txtD").value;
+		var padraoD = /^MT-\d{2}\.\d{3}-IFTM$/i;
+		if (padraoD.test(dadoD)){
+			document.getElementById("mensagemD").innerHTML=" = Válido!";
+			document.getElementById("mensagemD").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemD").innerHTML=" = Inválido!";
+			document.getElementById("mensagemD").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA E*******************************************************************/
 //Único formato permitido: MT-xx.xxx-IFTM. Onde x deverá um dígito, 
@@ -86,16 +111,22 @@ document.getElementById("txtE").onfocus = function() {
     document.getElementById("mensagemE").innerHTML="";
 }
 document.getElementById("validaE").onclick = function() {
-    var dadoE = document.getElementById("txtE").value;
-    var padraoE = /^MT-\d{2}\.\d{3}-(I|i)(F|f)(T|t)(M|m)$/;
-    if (padraoE.test(dadoE)){
-        document.getElementById("mensagemE").innerHTML=" = Válido!";
-        document.getElementById("mensagemE").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemE").innerHTML=" = Inválido!";
-        document.getElementById("mensagemE").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaE").checked==false){
+		document.getElementById("validaE").checked=false;
+		document.getElementById("mensagemE").innerHTML="";
+	}
+	else{
+		var dadoE = document.getElementById("txtE").value;
+		var padraoE = /^MT-\d{2}\.\d{3}-(I|i)(F|f)(T|t)(M|m)$/;
+		if (padraoE.test(dadoE)){
+			document.getElementById("mensagemE").innerHTML=" = Válido!";
+			document.getElementById("mensagemE").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemE").innerHTML=" = Inválido!";
+			document.getElementById("mensagemE").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA F*******************************************************************/
 //Único formato permitido: MT-xx.xxx-IFTM. Onde x é um dígito e 
@@ -107,16 +138,22 @@ document.getElementById("txtF").onfocus = function() {
     document.getElementById("mensagemF").innerHTML="";
 }
 document.getElementById("validaF").onclick = function() {
-    var dadoF = document.getElementById("txtF").value;
-    var padraoF = /^M\s?T-\d{2}\.\d{3}-I\s?F\s?T\s?M$/i;
-    if (padraoF.test(dadoF)){
-        document.getElementById("mensagemF").innerHTML=" = Válido!";
-        document.getElementById("mensagemF").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemF").innerHTML=" = Inválido!";
-        document.getElementById("mensagemF").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaF").checked==false){
+		document.getElementById("validaF").checked=false;
+		document.getElementById("mensagemF").innerHTML="";
+	}
+	else{
+		var dadoF = document.getElementById("txtF").value;
+		var padraoF = /^M\s?T-\d{2}\.\d{3}-I\s?F\s?T\s?M$/i;
+		if (padraoF.test(dadoF)){
+			document.getElementById("mensagemF").innerHTML=" = Válido!";
+			document.getElementById("mensagemF").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemF").innerHTML=" = Inválido!";
+			document.getElementById("mensagemF").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA G*******************************************************************/
 //Único formato permitido: MT-xx.xxx-IFTM Y. Onde x é um dígito, 
@@ -129,16 +166,22 @@ document.getElementById("txtG").onfocus = function() {
     document.getElementById("mensagemG").innerHTML="";
 }
 document.getElementById("validaG").onclick = function() {
-    var dadoG = document.getElementById("txtG").value;
-    var padraoG = /^M\s?T-\d{2}\.\d{3}-I\s?F\s?T\s?M Uberlândia( Centro)?$/i;
-    if (padraoG.test(dadoG)){
-        document.getElementById("mensagemG").innerHTML=" = Válido!";
-        document.getElementById("mensagemG").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemG").innerHTML=" = Inválido!";
-        document.getElementById("mensagemG").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaG").checked==false){
+		document.getElementById("validaG").checked=false;
+		document.getElementById("mensagemG").innerHTML="";
+	}
+	else{
+		var dadoG = document.getElementById("txtG").value;
+		var padraoG = /^M\s?T-\d{2}\.\d{3}-I\s?F\s?T\s?M Uberlândia( Centro)?$/i;
+		if (padraoG.test(dadoG)){
+			document.getElementById("mensagemG").innerHTML=" = Válido!";
+			document.getElementById("mensagemG").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemG").innerHTML=" = Inválido!";
+			document.getElementById("mensagemG").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA H*******************************************************************/
 //Único formato permitido: +yy(xx)xxxxx-xxxx. Onde x e y são dígitos. 
@@ -148,16 +191,22 @@ document.getElementById("txtH").onfocus = function() {
     document.getElementById("mensagemH").innerHTML="";
 }
 document.getElementById("validaH").onclick = function() {
-    var dadoH = document.getElementById("txtH").value;
-    var padraoH = /^\+\d{1,3}\(\d{2}\)\d{5}-\d{4}$/;
-    if (padraoH.test(dadoH)){
-        document.getElementById("mensagemH").innerHTML=" = Válido!";
-        document.getElementById("mensagemH").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemH").innerHTML=" = Inválido!";
-        document.getElementById("mensagemH").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaH").checked==false){
+		document.getElementById("validaH").checked=false;
+		document.getElementById("mensagemH").innerHTML="";
+	}
+	else{
+		var dadoH = document.getElementById("txtH").value;
+		var padraoH = /^\+\d{1,3}\(\d{2}\)\d{5}-\d{4}$/;
+		if (padraoH.test(dadoH)){
+			document.getElementById("mensagemH").innerHTML=" = Válido!";
+			document.getElementById("mensagemH").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemH").innerHTML=" = Inválido!";
+			document.getElementById("mensagemH").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA I*******************************************************************/
 //Únicos formatos permitidos: (xx)xxxxx-xxxx ou (xxx)xxxxx-xxxx
@@ -166,16 +215,22 @@ document.getElementById("txtI").onfocus = function() {
     document.getElementById("mensagemI").innerHTML="";
 }
 document.getElementById("validaI").onclick = function() {
-    var dadoI = document.getElementById("txtI").value;
-    var padraoI = /^\(\d{2,3}\)\d{5}-\d{4}$/;
-    if (padraoI.test(dadoI)){
-        document.getElementById("mensagemI").innerHTML=" = Válido!";
-        document.getElementById("mensagemI").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemI").innerHTML=" = Inválido!";
-        document.getElementById("mensagemI").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaI").checked==false){
+		document.getElementById("validaI").checked=false;
+		document.getElementById("mensagemI").innerHTML="";
+	}
+	else{
+		var dadoI = document.getElementById("txtI").value;
+		var padraoI = /^\(\d{2,3}\)\d{5}-\d{4}$/;
+		if (padraoI.test(dadoI)){
+			document.getElementById("mensagemI").innerHTML=" = Válido!";
+			document.getElementById("mensagemI").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemI").innerHTML=" = Inválido!";
+			document.getElementById("mensagemI").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA J*******************************************************************/
 //O faturamento de uma empresa pode variar de R$0,00 até R$999.999.999.999,99 (999 bilhões de reais).
@@ -185,17 +240,23 @@ document.getElementById("txtJ").onfocus = function() {
     document.getElementById("mensagemJ").innerHTML="";
 }
 document.getElementById("validaJ").onclick = function() {
-    var dadoJ = document.getElementById("txtJ").value;
-    //var padraoJ = /^R\$\d{1,3}(\.\d{3})*,\d{2}$/; //qq valor
-    var padraoJ = /^R\$\d{0,3}(\.\d{3}){0,3},\d{1,2}$/;//até 999 bilhoes
-    if (padraoJ.test(dadoJ)){
-        document.getElementById("mensagemJ").innerHTML=" = Válido!";
-        document.getElementById("mensagemJ").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemJ").innerHTML=" = Inválido!";
-        document.getElementById("mensagemJ").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaJ").checked==false){
+		document.getElementById("validaJ").checked=false;
+		document.getElementById("mensagemJ").innerHTML="";
+	}
+	else{
+		var dadoJ = document.getElementById("txtJ").value;
+		//var padraoJ = /^R\$\d{1,3}(\.\d{3})*,\d{2}$/; //qq valor
+		var padraoJ = /^R\$\d{0,3}(\.\d{3}){0,3},\d{1,2}$/;//até 999 bilhoes
+		if (padraoJ.test(dadoJ)){
+			document.getElementById("mensagemJ").innerHTML=" = Válido!";
+			document.getElementById("mensagemJ").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemJ").innerHTML=" = Inválido!";
+			document.getElementById("mensagemJ").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA K*******************************************************************/
 //HH:MM:SS:CC (HH: hora, MM: minuto, SS: segundo, CC: centésimo de segundo).
@@ -206,16 +267,22 @@ document.getElementById("txtK").onfocus = function() {
     document.getElementById("mensagemK").innerHTML="";
 }
 document.getElementById("validaK").onclick = function() {
-    var dadoK = document.getElementById("txtK").value;
-    var padraoK = /^([0-1][0-9]|2[0-3])(:([0-5][0-9])){2}:([0-9][0-9])$/;
-    if (padraoK.test(dadoK)){
-        document.getElementById("mensagemK").innerHTML=" = Válido!";
-        document.getElementById("mensagemK").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemK").innerHTML=" = Inválido!";
-        document.getElementById("mensagemK").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaK").checked==false){
+		document.getElementById("validaK").checked=false;
+		document.getElementById("mensagemK").innerHTML="";
+	}
+	else{
+		var dadoK = document.getElementById("txtK").value;
+		var padraoK = /^([0-1][0-9]|2[0-3])(:([0-5][0-9])){2}:([0-9][0-9])$/;
+		if (padraoK.test(dadoK)){
+			document.getElementById("mensagemK").innerHTML=" = Válido!";
+			document.getElementById("mensagemK").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemK").innerHTML=" = Inválido!";
+			document.getElementById("mensagemK").style.backgroundColor = "red";
+		}
+	}
 };
 /***LETRA L*******************************************************************/
 //Únicos formatos permitidos: X&W.Y.Z-U,V.T ou X&W.Y-U,V.T Onde X deverá ser
@@ -232,16 +299,22 @@ document.getElementById("txtL").onfocus = function() {
     document.getElementById("mensagemL").innerHTML="";
 }
 document.getElementById("validaL").onclick = function() {
-    var dadoL = document.getElementById("txtL").value;
-    var padraoL = /^$/;
-    if (padraoL.test(dadoL)){
-        document.getElementById("mensagemL").innerHTML=" = Válido!";
-        document.getElementById("mensagemL").style.backgroundColor = "#99ff99";
-    }
-    else{
-        document.getElementById("mensagemL").innerHTML=" = Inválido!";
-        document.getElementById("mensagemL").style.backgroundColor = "#ffb3b3";
-    }
+	if (document.getElementById("validaL").checked==false){
+		document.getElementById("validaL").checked=false;
+		document.getElementById("mensagemL").innerHTML="";
+	}
+	else{
+		var dadoL = document.getElementById("txtL").value;
+		var padraoL = /^$/;
+		if (padraoL.test(dadoL)){
+			document.getElementById("mensagemL").innerHTML=" = Válido!";
+			document.getElementById("mensagemL").style.backgroundColor = "green";
+		}
+		else{
+			document.getElementById("mensagemL").innerHTML=" = Inválido!";
+			document.getElementById("mensagemL").style.backgroundColor = "red";
+		}
+	}
 };
 /*
 X&W.Y.Z-U,V.T ou X&W.Y-U,V.T 
