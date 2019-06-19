2ce9d140f5023fb058c1581bdbefb976d61fa18c
cavanhaman <cavanhaman@hotmail.com>
2019-06-19 19:46:08 -0300

    Avaliacao feita em sala de aula - cronometro com insercao de valor

--- Ex05/avaliacao.js
+++ Ex05/avaliacao.js
@@ -1,29 +1,32 @@
-var cx= 0;
-var s = 0;
-var m = 0;
-var h = 0;
-var pausado;
-
-var dhor = document.getElementById("hh");
-var dmin = document.getElementById("mm");
-var dseg = document.getElementById("ss");
+var dhor = document.getElementById("txtH");
+var dmin = document.getElementById("txtM");
+var dseg = document.getElementById("txtS");
 
 var play = document.getElementById("play");
 var stop = document.getElementById("stop");
 var pause = document.getElementById("pause");
 
+var s;
+var m;
+var h;
+var pausado;
 
 function cronometro(){
     pausado=setTimeout("contaTempo()",1000);
 }
 
 function contaTempo() {    
+	h=dhor.value;
+	m=dmin.value;
+	s=dseg.value;
+
 	s++;
+	if (s>=60 || m>=60 || h>=24){s=0; m=0; h=0;}
 	if (s == 60)  { m++; s=0; }
 	if (m == 60)  { h++; s=0; m=0; }
-	if (h < 10) dhor.innerHTML = "0"+h; else dhor.innerHTML = h;
-	if (s < 10) dseg.innerHTML = "0"+s; else dseg.innerHTML = s;
-	if (m < 10) dmin.innerHTML = "0"+m; else dmin.innerHTML = m;
+	if (h < 10) dhor.value = "0"+h; else dhor.value = h;
+	if (s < 10) dseg.value = "0"+s; else dseg.value = s;
+	if (m < 10) dmin.value = "0"+m; else dmin.value = m;
 	cronometro();
 }
 
@@ -53,9 +56,7 @@ function crStop(){
     m=0;
     s=0;
 
-    dhor.innerHTML="00";
-    dmin.innerHTML="00";
-    dseg.innerHTML="00";
+    dhor.value="00";
+    dmin.value="00";
+    dseg.value="00";
 }
-
-/*window.onload=cronometro;*/
