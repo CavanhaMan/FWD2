75f66b76bf9cb8847b78275219fb991480c13bad
CavanhaMan <cavanhaman@hotmail.com>
2019-06-19 11:11:02 -0300

    Temporizador com troca de fotos finalizado

--- Ex05/trocafotos.js
+++ Ex05/trocafotos.js
@@ -4,10 +4,52 @@ building[1] = "logo4.jpg";
 building[2] = "logo5.jpg";
 building[3] = "logo6.jpg";
 var num = 0;
-     
-function changepic(){
-    if (num>=building.length-1)
-        num=0;
-        num=num+1;
-        document.buildingpic.src=building[num];
+var pausado=0;
+
+var play = document.getElementById("play");
+var stop = document.getElementById("stop");
+var pause = document.getElementById("pause");
+var next = document.getElementById("next");
+var back = document.getElementById("back");
+
+function changePic(){
+    if (num>=building.length-1) num=0;
+    num++;
+    document.buildingpic.src=building[num];
+}
+
+function changePicBack(){
+    if (num<=0) num=4;
+    num--;
+    document.buildingpic.src=building[num];
+}
+
+function cronometro(){
+    setInterval(function() {
+        if(pausado==0)
+            changePic()
+    },1000);
+}
+
+function crPlay(){
+    if (pausado==0) cronometro();
+    else pausado=0;
+    play.disabled = true;
+    pause.disabled = false;
+    stop.disabled = false;
+}
+
+function crPause(){
+    pausado=1;
+    play.disabled = false;
+    pause.disabled = true;
+    stop.disabled = false;
+}
+
+function crStop(){
+    pausado=1;
+    document.buildingpic.src=building[0];
+    play.disabled = false;
+    pause.disabled = true;
+    stop.disabled = true;
 }
