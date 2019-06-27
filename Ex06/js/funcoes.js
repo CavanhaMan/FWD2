718b2b8f1b7284429822836031a53bd6da66e9cf
cavanhaman <cavanhaman@hotmail.com>
2019-06-26 21:25:54 -0300

    Melhorias no layout e novas estrelas

--- Ex06/js/funcoes.js
+++ Ex06/js/funcoes.js
@@ -1,7 +1,7 @@
 //Rodrigo Cavanhaman
 
 function trocarImg(nImg,novaLegenda,corLegenda){
-    document.getElementById("imgStar").src="img/star"+nImg+".png";
+    document.getElementById("imgStar").src="img/star_"+nImg+".png";
     document.getElementById("legenda").innerHTML="Estrela " + novaLegenda;
     document.getElementById("legenda").style.color=corLegenda;
 }
