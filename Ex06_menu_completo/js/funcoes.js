243a6aaaeb810601763bb856b21b7c2da503ee30
CavanhaMan <cavanhaman@hotmail.com>
2019-06-29 23:44:30 -0300

    Layout finalizado

--- Ex06_menu_completo/js/funcoes.js
+++ Ex06_menu_completo/js/funcoes.js
@@ -1,7 +1,6 @@
 //Rodrigo Cavanhaman
 
-function trocarImg(nImg,novaLegenda,corLegenda){
-    document.getElementById("imgCurso").src="img/beardpc_"+nImg+".jpg";
-    document.getElementById("legenda").innerHTML="Estrela " + novaLegenda;
-    document.getElementById("legenda").style.color=corLegenda;
+function trocarImg(curso,nome){
+    document.getElementById("imgCurso").src="img/beardpc_"+curso+".jpg";
+    document.getElementById("legenda").innerHTML=nome;
 }
