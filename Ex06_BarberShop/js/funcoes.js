abcff5a682d37d42d238bc736c19ea36ac5c4cce
cavanhaman <cavanhaman@hotmail.com>
2019-07-01 15:59:22 -0300

    Adicionando texto lateral e outras modificacoes de layout

--- Ex06_BarberShop/js/funcoes.js
+++ Ex06_BarberShop/js/funcoes.js
@@ -3,5 +3,6 @@
 function trocarImg(num){
     document.getElementById("barber").src="img/barbearia"+num+".jpg";
     document.getElementById("botoes").src="img/buttons"+num+".png";
-    document.getElementById("legenda").innerHTML=document.getElementById("leg"+num).innerHTML;
+    document.getElementById("icones").src="img/icone"+num+".png";
+    document.getElementById("textoleg").innerHTML=document.getElementById("leg"+num).innerHTML;
 }
