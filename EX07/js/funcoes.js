18d9391caab2dd663a7f144d44a5f672b06e8902
cavanhaman <cavanhaman@hotmail.com>
2019-07-03 19:42:31 -0300

    Continuando exercicio Joaozinho

--- EX07/js/funcoes.js
+++ EX07/js/funcoes.js
@@ -1,5 +1,36 @@
 //Rodrigo Cavanhaman
+var joao = document.getElementById("joaozinho");
+console.log(joao);
+var msg = document.getElementById("mensagem");
+console.log(msg);
+var timer=0;
+var dude=0;
 
-moleque = document.getElementById("joaozinho");
+function over(){
+    joao.src="Imagens/Assustado.png";
+    dude="assustado";
+    if(dude=="assustado")
+        setTimeout(function(){ joao.src="Imagens/Pensativo.png"; }, 4000);
+    if(dude=="alegre")
+    else
+        clearTimeout;
+}
 
-moleque.onmouseenter
+function click(){
+    if(dude=="alegre"){
+        msg.innerHTML="Que bom que você voltou.<BR>Vamos brincar?";
+        dude="alegre";
+    }
+    else{
+        joao.src="Imagens/Alegre.png";
+        dude="alegre";
+        msg.innerHTML="Gosto quando você<BR>interage comigo";
+        setTimeout(function(){ msg.innerHTML=""; }, 4000);
+    }
+}
+
+function leave(){
+    
+}
+
+// setTimeout(function(){ alert("Hello"); }, 3000);
