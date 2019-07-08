cb564d240e3e61e331937dd30e2551a554125172
CavanhaMan <cavanhaman@hotmail.com>
2019-07-08 01:04:51 -0300

    Organizando funcoes

--- EX06_Joaozinho/js/funcoes.js
+++ EX06_Joaozinho/js/funcoes.js
@@ -16,19 +16,13 @@ function over(){
     if (dude=="pensativo"){
         joao.src="Imagens/Assustado.png";
         dude="assustado";
-        console.log(dude);
-    }
-    //- mouse over ASSUSTADO e não clicar => deverá virar PENSATIVO
-    else if(dude=="assustado"){
-        joao.src="Imagens/Pensativo.png";
-        dude="pensativo";
-        console.log(dude);
+        console.log("dude_over_pensativo: ",dude);
     }
     //- mouse over ALEGRE => mensagem "Que bom que você voltou. Vamos brincar?" (IMAGEM NAO MUDA)
     else if(dude=="alegre"){
+		clearTimeout(timer);
         msg.innerHTML="Que bom que você voltou.<BR>Vamos brincar?";
-        timer();
-        console.log(dude);
+        console.log("dude_over_alegre: ",dude);
     }
     //- mouse over NERVOSO => mensagem "Não suma mais. Gosto quando você interage comigo" =>
     //                     => depois deverá virar ALEGRE
@@ -36,18 +30,43 @@ function over(){
         msg.innerHTML="Não suma mais. Gosto quando você interage comigo";
         joao.src="Imagens/Alegre.png";
         dude="alegre";
-        console.log(dude);
+        console.log("dude_over_nervoso: ",dude);
+    }
+    //- mouse over ASSUSTADO e não clicar => deverá virar PENSATIVO
+    else if(dude=="assustado"){
+        joao.src="Imagens/Pensativo.png";
+        dude="pensativo";
+        console.log("dude_over_assustado: ",dude);
     }
 }
+
 function click(){
     //- clicar na ASSUSTADO => deverá virar ALEGRE + mensagem "Gosto quando você interage comigo" (4 segundos)
     if(dude=="assustado"){
         joao.src="Imagens/Alegre.png";
         dude="alegre";
         setTimeout(function(){ msg.innerHTML="Gosto quando você interage comigo"; }, 4000);
-        console.log(dude);
+        console.log("dude_clicado_assustado: ",dude);
     }
 }
+
+function leave(){
+    if(dude=="alegre"){
+        timer = setTimeout(function(){
+			setTimeout(msg.innerHTML="",4000);
+			joao.src="Imagens/Nervoso.png";
+			dude="nervoso";
+			console.log("dude_over_alegre: ",dude);
+		}, 5000);
+    }
+    else if(dude=="assustado"){
+		g_personagem = "Pensativo";
+        joao.src="Imagens/Pensativo.png";
+        dude="pensativo";
+    }
+}
+
+
 function timer(){
     //- ALEGRE sem clicar nem passar num intervalo de 5 e 15 segundos => NERVOSO + mensagem "Onde está você? Brinque comigo!!!"
     var x =(Math.floor(Math.random()*15)+5);
