148ad693a924f7a38df2ecae8e6d2c7d03c60068
CavanhaMan <cavanhaman@hotmail.com>
2019-07-10 10:45:14 -0300

    Correcao em bugs e funcionalidade ok

--- EX06_Breno/bkp/js/funcoes.js
+++ EX06_Breno/bkp/js/funcoes.js
@@ -1,11 +1,12 @@
 //Rodrigo Cavanhaman
 var joao = document.getElementById("joaozinho");
+console.log(joao);
 var msg = document.getElementById("mensagem");
+console.log(msg);
 var aviso = document.getElementById("aviso");
-var chat = document.getElementById("chat");
 var timer;
+var control=0;
 var aux; 
-var z;
 var brabo;
 
 /* Inicialmente garoto “Pensativo”*/
@@ -15,10 +16,7 @@ console.log(dude=="Pensativo");
 
 function dudeOver(){
     clearTimeout(brabo);
-    clearTimeout(timer);
-    clearTimeout(z);
-    aviso.style.visibility = "hidden";
-    joao.className = "normal";
+
     //- mouse over Pensativo => deverá virar Assustado
     if (dude=="Pensativo"){
         joao.src="Imagens/Assustado.png";
@@ -27,20 +25,26 @@ function dudeOver(){
     }
     //- mouse over Alegre => mensagem "Que bom que você voltou. Vamos brincar?" (IMAGEM NAO MUDA)
     else if(dude=="Alegre"){
-        chat.src="Imagens/f_quebom.png";
+        msg.innerHTML="Que bom que você voltou.<BR>Vamos brincar?";
         clearTimeout(timer);
-        timer = setTimeout(function(){ chat.src="Imagens/f_quebom.png"; }, 4000);
-         console.log("dude_over_Alegre: ",dude);
+        timer = setTimeout(function(){ msg.innerHTML=""; }, 4000);
+    //     console.log("dude_over_Alegre: ",dude);
     }
     //- mouse over Nervoso => mensagem "Não suma mais. Gosto quando você interage comigo" =>
     //                     => depois deverá virar Alegre
     else if(dude=="Nervoso"){
-        chat.src="Imagens/f_naosuma.png";
-        timer = setTimeout(function(){ chat.src=""; }, 4000);
+        clearTimeout(timer);
+        msg.innerHTML="Não suma mais! Gosto quando você interage comigo";
         joao.src="Imagens/Alegre.png";
         dude="Alegre";
          console.log("dude_over_Nervoso: ",dude);
     }
+    //- mouse over Assustado e não clicar => deverá virar Pensativo
+/*    else if(dude=="Assustado"){
+        joao.src="Imagens/Pensativo.png";
+        dude="Pensativo";
+        console.log("dude_over_Assustado: ",dude);
+    }*/
 }
 
 function dudeClick(){
@@ -50,8 +54,8 @@ function dudeClick(){
         console.log("dude_clique_Assustado => ",dude);
         joao.src="Imagens/Alegre.png";
         dude="Alegre";
-        chat.src="Imagens/f_gosto.png";
-        timer = setTimeout(function(){ chat.src=""; }, 4000);
+        msg.innerHTML="Gosto quando você interage comigo!";
+        timer = setTimeout(function(){ msg.innerHTML=""; }, 4000);
         console.log("dude_clicado_Assustado: ",dude);
     }
 }
@@ -64,29 +68,46 @@ function dudeOut(){
     }
     //- Alegre sem clicar nem passar num intervalo de 5 e 15 segundos => Nervoso + mensagem "Onde está você? Brinque comigo!!!"
     if (dude=="Alegre"){
-        clearTimeout(z);
         var x =(Math.floor(Math.random()*15)+5);
         aux=x-1;
         console.log("tempo para ficar nervoso: "+x)
-        brabo = setInterval(function(){aviso.innerHTML=("CUIDADO! Ficando nervoso em... "+aux--);},1000);
-        aviso.style.visibility = "visible";
+        brabo = setInterval(function(){
+            aviso.innerHTML=("CUIDADO! Ficando nervoso em... "+aux--);
+        },1000);
         timer = setTimeout(function(){ 
             clearTimeout(brabo);
             dude="Nervoso";
             joao.src="Imagens/Nervoso.png";
-            chat.src="Imagens/f_ondeesta.png";
-            timer = setTimeout(function(){ chat.src=""; }, 4000);
+            msg.innerHTML="Onde está você?<BR>Brinque comigo!!!"; 
         }, x*1000);
-        z = setTimeout(function(){ 
-            aviso.style.visibility = "hidden";
-            joao.src="Imagens/Irado.png";
-            chat.src="Imagens/f_gr.png";
-            joao.className = "irado";
-        }, x*2000);
     }
+    // if(dude=="Alegre"){
+    //     timer = setTimeout(function(){
+	// 		setTimeout(msg.innerHTML="",4000);
+	// 		joao.src="Imagens/Nervoso.png";
+	// 		dude="Nervoso";
+	// 		console.log("dude_over_Alegre: ",dude);
+	// 	}, 5000);
+    // }
+    // else if(dude=="Assustado"){
+	// 	g_personagem = "Pensativo";
+    //     joao.src="Imagens/Pensativo.png";
+    //     dude="Pensativo";
+    // }
 }
 
 
+
+
+/******************************************* */
+//        setTimeout(function(){ joao.src="Imagens/Pensativo.png"; }, 4000);
+//        clearTimeout();
+
+
+
+function leave(){
+    control=1;
+}
 /**********************************************************************/
 
 //- mouse over Pensativo => deverá virar Assustado
