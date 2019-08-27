967ef4a49adec01851e8faa3e97fcfacf9cc90d8
CavanhaMan <cavanhaman@hotmail.com>
2019-08-26 21:29:36 -0300

    Adicionando novos arquivos no index e no javascript

--- Ex02_sorteio/script.js
+++ Ex02_sorteio/script.js
@@ -2,7 +2,8 @@
 $(document).ready(function (){
 	var mostraex1="1. Construa uma página para a Internet capaz de gerar e mostrar os seis números da mega sena para uma pessoa. Os números deverão ser exibidos no corpo do documento HTML ao usuário, após o carregamento da página.";
 	var mostraex2="2. Crie uma página web capaz de sortear a ordem de apresentação de um grupo de pessoas a partir das seguintes informações: número de pessoas e o nome de cada uma delas. Após a entradas destas informações, a página deverá ser capaz de exibir ao usuário, no corpo do documento, a sequência das apresentações, conforme mostrado na imagem.";
-	
+	var mostraex3="3. Crie uma página web para sortear as cartas do jogo TRUCO para os 4 jogadores. Devem ser sorteadas 12 cartas, 3 para cada jogador"
+
 	$('button').hover(
 		function(){$(this).css("background-color", "#ffdb4d");}, 
 		function(){$(this).css("background-color", "#997a00");}
@@ -21,6 +22,13 @@ $(document).ready(function (){
 		document.getElementById("imgshow").src=""; 
 		$("#mostraex").text("");
 		$("#framehtml").attr("src", "JavaScript_-_Lista_de_exercicios_02.pdf");
+	});
+
+	//FUNÇÃO EXERCÍCIOS EXTRA
+	$('#exE').click(function(e){
+		document.getElementById("imgshow").src=""; 
+		$("#mostraex").text("");
+		$("#framehtml").attr("src", "JavaScript_-_Lista_de_exercicios_02a.pdf");
 	});
 
 	//FUNÇÃO MEGASENA
@@ -42,6 +50,23 @@ $(document).ready(function (){
 	});
 	//$('#ex2').mouseover(function(e){$("#frametxt").attr("src", "SorteioTxt.html");});
 
+	//FUNÇÃO SORTEIO SIMPLIFICADA
+	$('#ex2a').click(function(e){
+		document.getElementById("imgshow").src="sorteio.png"; 
+		//document.getElementById("imgmega").style.visibility = "hidden"; 
+		$("#mostraex").text(mostraex2);
+		$("#framehtml").attr("src", "SorteioEasy.html");
+		clicado=2;
+	});
+
+	//FUNÇÃO BARALHO TRUCO
+	$('#ex3').click(function(e){
+		document.getElementById("imgshow").src="truco.png"; 
+		//document.getElementById("imgmega").style.visibility = "hidden"; 
+		$("#mostraex").text(mostraex3);
+		$("#framehtml").attr("src", "Truco.html");
+		clicado=3;
+	});
 
 });
 //<img id="imgmega" src="megasena.png" alt="megasena">
