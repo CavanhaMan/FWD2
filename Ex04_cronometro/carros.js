eb0f49b32c55c4efd28e58973d999a76967f9a3c
CavanhaMan <cavanhaman@hotmail.com>
2019-11-23 09:16:08 -0300

    Segunda versao exercicio carros FUNCIONANDO

--- Ex04_cronometro/carros.js
+++ Ex04_cronometro/carros.js
@@ -1,21 +1,25 @@
 var num = 0;
 
-var carros = [][];
-carros[0][0] = "car0.jpg"
-carros[0][1] = "car1.jpg"
-carros[0][2] = "car2.jpg"
-carros[1][0] = "Civic";
-carros[1][1] = "Corola";
-carros[1][2] = "Focus";
-carros[2][0] = "140 cv";
-carros[2][1] = "155 cv";
-carros[2][2] = "172 cv";
-carros[3][0] = "3 anos";
-carros[3][1] = "3 anos";
-carros[3][2] = "5 anos";
-carros[4][0] = "9,2 km/l";
-carros[4][1] = "não informado";
-carros[4][2] = "8,5 km/l";
+var carros0 = [];
+var carros1 = [];
+var carros2 = [];
+var carros3 = [];
+var carros4 = [];
+carros0[0] = "car0.jpg"
+carros0[1] = "car1.jpg"
+carros0[2] = "car2.jpg"
+carros1[0] = "Civic";
+carros1[1] = "Corola";
+carros1[2] = "Focus";
+carros2[0] = "140 cv";
+carros2[1] = "155 cv";
+carros2[2] = "172 cv";
+carros3[0] = "3 anos";
+carros3[1] = "3 anos";
+carros3[2] = "5 anos";
+carros4[0] = "9,2 km/l";
+carros4[1] = "não informado";
+carros4[2] = "8,5 km/l";
 
 var imag = document.getElementById("imag");
 var vnome = document.getElementById("nome");
@@ -30,10 +34,10 @@ setInterval(function () {
 function mudaCarro() {
     if (num >= 2) num = 0;
     else num++;
-    imag.src = carros[0][num];
-    vnome.innerHTML = carros[1][num];
-    vpote.innerHTML = carros[2][num];
-    vgara.innerHTML = carros[3][num];
-    vcons.innerHTML = carros[4][num];
+    imag.src = carros0[num];
+    vnome.innerHTML = carros1[num];
+    vpote.innerHTML = carros2[num];
+    vgara.innerHTML = carros3[num];
+    vcons.innerHTML = carros4[num];
 
 }
