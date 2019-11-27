92e2cfda0d198c5158c0bcd0a7ea4ec26f6c3c1c
CavanhaMan <cavanhaman@hotmail.com>
2019-11-27 10:04:55 -0300

    Pacman com posicao aleatoria

--- Ex11_Colisão/index.js
+++ Ex11_Colisão/index.js
@@ -1,48 +1,23 @@
-var w = window.innerHeight;
-console.log("altura tela: "+w)
-var h = window.innerWidth;
-console.log("altura tela: "+h)
+window.onLoad = Prep();
 
-var topo = parseInt(Math.random() * (w - 100));
-console.log("topo: "+topo);
-var lado = parseInt(Math.random() * (h - 100));
-console.log("lado: "+lado);
+function Prep() {
+  h = window.innerHeight;
+  w = window.innerWidth;
 
-//CRIANDO IMAGEM PACMAN
-var img1 = document.createElement('img');
-img1.src = 'pacman6b.gif';
-img1.id = 'pacman';
+  pacman = document.getElementById("pacman");
+  pacmanH = pacman.clientHeight;
+  pacmanW = pacman.clientWidth;
 
-img1.setAttribute('top',topo);
-img1.setAttribute('left',lado);
+  espacoV = h - pacmanH;
+  espacoH = w - pacmanW;
 
-document.getElementById('header').appendChild(img1)
-var pacman = document.getElementById("pacman");
-console.log(pacman);
-var objPacman = pacman.getBoundingClientRect();
-console.log(objPacman);
+  movepacman();
+}
 
-pacman.style.top = topo+"px";
-pacman.style.left = lado+"px";
+function movepacman() {
+  var posicaoV = Math.round(Math.random() * espacoV);
+  var posicaoH = Math.round(Math.random() * espacoH);
 
-
-//CRIANDO IMAGEM FANTASMA
-var img2 = document.createElement('img');
-img2.src = 'fantasma1.png';
-img2.id = 'fantasma';
-document.getElementById('header').appendChild(img2)
-var fantasma = document.getElementById("fantasma");
-console.log(fantasma);
-var objFantasma = fantasma.getBoundingClientRect();
-console.log(objFantasma);
-
-
-/*var larguraPac = pacman.clientWidth;
-console.log("largura: "+larguraPac);
-var alturaPac = pacman.clientHeight;
-console.log("altura: "+alturaPac);*/
-
-//var css = 'position:absolute; top:400px; left:${left}px; border:1px solid red;';
-//pacman.setAttribute("style",css);
-//pacman.style = css;
-//document.getElementsByTagName("body")[0].appendChild(pacman); 
+  pacman.style.top = posicaoV + "px";
+  pacman.style.left = posicaoH + "px";
+}
