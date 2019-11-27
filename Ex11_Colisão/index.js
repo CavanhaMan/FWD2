e8c7c715322d76db67212a731323b467ece7a571
CavanhaMan <cavanhaman@hotmail.com>
2019-11-26 21:24:45 -0300

    Declarando objeto e variaveis iniciais

--- Ex11_Colisão/index.js
+++ Ex11_Colisão/index.js
@@ -1,8 +1,25 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+var pacman = document.getElementById("pacman");
+console.log(pacman);
+var objPacman = pacman.getBoundingClientRect();
+console.log(objPacman);
+var fantasma = document.getElementById("fantasma");
+console.log(fantasma);
+var objFantasma = fantasma.getBoundingClientRect();
+console.log(objFantasma);
+
+posicionaPacman();
+
+function posicionaPacman(){
+  var larguraPac = pacman.clientWidth;
+  console.log(larguraPac);
+  var alturaPac  = pacman.clientHeight;
+  console.log(alturaPac);
+  var left = parseInt(Math.random() * (window.innerWidth - larguraPac));
+  console.log(left);
+  var top  = parseInt(Math.random() * (window.innerHeight - alturaPac));
+  console.log(top);
+  var css = 'position:absolute; top:${top}px; left:${left}px';
+  pacman.setAttribute("style",css);
+  //pacman.style = css;
+  document.getElementsByTagName("body")[0].appendChild(pacman);
 }
