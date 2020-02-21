b46001879db7102546de9b42d3f0f4d79e9095f2
CavanhaMan <cavanhaman@hotmail.com>
2020-02-20 22:06:51 -0300

    adicionando mudanca de arquivos html

--- CV_Web/js/funcoes.js
+++ CV_Web/js/funcoes.js
@@ -1,7 +1,6 @@
 //Rodrigo Cavanhaman
 
 function trocarPag(num){
-    document.getElementById("barber").src="conteudo/curric"+num+".html";
     document.getElementById("botoes").src="img/buttons"+num+".png";
     document.getElementById("legenda").innerHTML=document.getElementById("leg"+num).innerHTML;
 }
