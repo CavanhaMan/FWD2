61ca2fa5813dd67866208964b4256c7783871430
CavanhaMan <cavanhaman@hotmail.com>
2020-02-20 23:44:39 -0300

    estudando iframe redimensionavel

--- CV_Web/js/funcoes.js
+++ CV_Web/js/funcoes.js
@@ -1,6 +1,7 @@
 //Rodrigo Cavanhaman
 
 function trocarPag(num){
+    $("#framehtml").attr("src", "conteudo/curric"+num+".html");
     document.getElementById("botoes").src="img/buttons"+num+".png";
     document.getElementById("legenda").innerHTML=document.getElementById("leg"+num).innerHTML;
 }
