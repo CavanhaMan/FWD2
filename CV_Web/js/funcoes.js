c9015947cc47b5c2fd52a66c8cc85972820d6f9c
CavanhaMan <cavanhaman@hotmail.com>
2020-02-21 03:07:04 -0300

    Testando codigo de menu

--- CV_Web/js/funcoes.js
+++ CV_Web/js/funcoes.js
@@ -1,7 +1,7 @@
 //Rodrigo Cavanhaman
 
-function trocarPag(num){
-    $("#framehtml").attr("src", "conteudo/curric"+num+".html");
-    document.getElementById("botoes").src="img/buttons"+num+".png";
-    document.getElementById("legenda").innerHTML=document.getElementById("leg"+num).innerHTML;
+function trocarPag(num) {
+    $("#iframe").attr("data", "conteudo/curric" + num + ".html");
+    document.getElementById("botoes").src = "img/buttons" + num + ".png";
+    document.getElementById("legenda").innerHTML = document.getElementById("leg" + num).innerHTML;
 }
