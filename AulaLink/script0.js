a2bfdc6b5b4e552ae19372fe9b48a4ad3934060c
CavanhaMan <cavanhaman@hotmail.com>
2021-02-16 08:58:43 -0300

    criação de 2 exemplos de cabecalho e colocando icone na pagina de exemplo

--- AulaLink/script0.js
+++ AulaLink/script0.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-	$('.test-area').mouseout(function(){
+	$('.hand').mouseout(function(){
 		$('#mycursor').hide();
 			return false;
 	});
-	$('.test-area').mouseenter(function(){
+	$('.hand').mouseenter(function(){
 		$('#mycursor').show();
 			return false;
 	});
-	$('.test-area').mousemove(function(e){
+	$('.hand').mousemove(function(e){
 		$('#mycursor').css('left', e.clientX - 20).css('top', e.clientY + 7);
 	});
 });
