4ca59649efa749047d9f3bd63fb7367ad25bbadc
cavanhaman <cavanhaman@hotmail.com>
2022-04-10 16:06:06 -0300

    botoes add carrinho e comprar funcionando

--- CavanhaShop/GridLayout2.js
+++ CavanhaShop/GridLayout2.js
@@ -5,6 +5,7 @@ function trocarImg2(num){
     document.getElementById("fotoproduto").src="img/bosch1"+num+".png";
 }
 
+/** BOTAO-SETINHAS PASSAR PROXIMAS FOTOS ***************************/
 $(document).ready(function(){
     $("#btprox").click(function(){
         $("#boxant").hide();
@@ -16,3 +17,16 @@ $(document).ready(function(){
     });
   });
 
+/** BOTAO COMPRAR E ADDICIONAR AO CARRINHO ***************************/
+$(document).ready(function(){
+    $("#comprar").click(function(){
+        alert("\nVoce comprou!\n\nObrigado!");
+    });
+
+    $("#addcarrinho").click(function(){
+        console.log("adicionando ao carrinho:")
+        var qtd = $("select.quant").children("option:selected").val();;
+        console.log("qtd="+qtd);
+        $("#numcarrinho").text(qtd);
+    });
+});
