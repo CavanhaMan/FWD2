6a5febb963b3db370e818ec89cd0dfa7016ee7d9
Rodrigo Cavanha <cavanhaman@hotmail.com>
2022-04-20 00:39:44 -0300

    adicionando conteudo ao site

---
+++ aula_SiteDinamico_conteudo/index.js
@@ -0,0 +1,9 @@
+/*function trocanoticia(num){
+    document.getElementById("noticia").src="noticia/n1.txt";
+}*/
+
+function trocanoticia(num){
+    document.getElementById("fotof").src="fotos/f" + num + ".png";
+    document.getElementById("titulot").src="titulos/t" + num + ".html";
+    document.getElementById("notician").src="noticias/n" + num + ".html";
+}
