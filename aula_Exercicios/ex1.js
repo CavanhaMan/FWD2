f663cb69e596990f3edaea55feef0e1e242afd17
Rodrigo Cavanha <cavanhaman@hotmail.com>
2022-04-24 23:11:58 -0300

    finalizando lista de exercicios

---
+++ aula_Exercicios/ex1.js
@@ -0,0 +1,29 @@
+/*** QUESTAO 1 ********************************************/
+
+//document.getElementById("total1").innerHTML="teste";
+/*
+var horas = $("#total1").closest('td').prev('td').text();
+console.log(horas);
+var valor = $("#total1").closest('td').prev('td').prev('td').text();
+console.log(valor);
+//valor = valor.replace('R$ ','');
+console.log(valor);
+var salario = parseInt(horas)*parseFloat(valor);
+console.log(salario);
+//salario = 'R$ ' + salario + ',00';
+console.log(salario);
+//document.getElementById("total1").innerHTML=salario;
+$("#total1").html(salario);
+*/
+$('document').ready(function(){
+    for (var i=1 ; i<=10 ; i++){
+        var horas = $("#total"+i).closest('td').prev('td').text();
+        //console.log(horas);
+        var valor = $("#total"+i).closest('td').prev('td').prev('td').text();
+        //console.log(valor);
+        var salario = parseFloat(horas)*parseFloat(valor);
+        //console.log(salario);
+        $("#total"+i).html(salario);
+    }
+    $('.tdr').mask('#.##0,00', {reverse: true});
+});
---
+++ aula_Exercicios/ex4.js
@@ -0,0 +1,37 @@
+/*** QUESTAO 4 ********************************************/
+function batg(){document.getElementById("batman").src="ex4/batman12g.png";}
+function bat(){document.getElementById("batman").src="ex4/batman12.png";}
+function wong(){document.getElementById("wwoman").src="ex4/wonderwoman8g.png";}
+function won(){document.getElementById("wwoman").src="ex4/wonderwoman8.png";}
+function supg(){document.getElementById("superm").src="ex4/superman2g.png";}
+function sup(){document.getElementById("superm").src="ex4/superman2.png";}
+
+function tresg(){
+    document.getElementById("batman").src="ex4/batman12g.png";
+    document.getElementById("wwoman").src="ex4/wonderwoman8g.png";
+    document.getElementById("superm").src="ex4/superman2g.png";
+}
+function tres(){
+    document.getElementById("batman").src="ex4/batman12.png";
+    document.getElementById("wwoman").src="ex4/wonderwoman8.png";
+    document.getElementById("superm").src="ex4/superman2.png";
+}
+
+/*** QUESTAO 4 ********************************************/
+function batg2(){document.getElementById("batman2").src="ex4/batman10g.png";}
+function bat2(){document.getElementById("batman2").src="ex4/batman10.png";}
+function wong2(){document.getElementById("wwoman2").src="ex4/wonderwoman4g.png";}
+function won2(){document.getElementById("wwoman2").src="ex4/wonderwoman4.png";}
+function supg2(){document.getElementById("superm2").src="ex4/superman4g.png";}
+function sup2(){document.getElementById("superm2").src="ex4/superman4.png";}
+
+function tresg2(){
+    document.getElementById("batman2").src="ex4/batman10g.png";
+    document.getElementById("wwoman2").src="ex4/wonderwoman4g.png";
+    document.getElementById("superm2").src="ex4/superman4g.png";
+}
+function tres2(){
+    document.getElementById("batman2").src="ex4/batman10.png";
+    document.getElementById("wwoman2").src="ex4/wonderwoman4.png";
+    document.getElementById("superm2").src="ex4/superman4.png";
+}
